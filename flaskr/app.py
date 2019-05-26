a07f7889b4af8808f956e2d3cea48dbdf700c4c5
Mayank Kapoor <mayankkapoormail@gmail.com>
2019-05-26 12:59:43 +0530

    Tutorial create server (#2)
    * First test of create_server
    
    * Update app.py and fix typo with True
    
    * Update app.py to create a volume first, then create server
    
    * Update app.py and remove pprint of volume object
    
    * Update app.py and remove pprint statements for server
    
    * Update app.py and add json.dumps to visualize Munch object
    
    * Update app.py and add pprint for json.dumps
    
    * Update app.py and replace pprint with print
    
    * Update README.md with create server instructions
    
    * Update README.md
    
    * Update app.py and fix typo

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from openstack import connection
 from pprint import pprint
+import json
 import os
 
 app = Flask(__name__)
@@ -37,6 +38,36 @@ def list_openstack_resources():
     pprint(user_list)
 
     return "List printed to stdout"
+
+
+@app.route("/create_server")
+def create_server():
+    # Check if connection is established
+    print("conn: ", conn)
+
+    # Create the volume first
+    volume_size = request.args.get('volume_size')
+    print("Starting to create volume with size (in GiB): ", volume_size)
+    volume = conn.create_volume(size=volume_size,
+                                image="cirros-0.4.0-x86_64-disk",
+                                wait=True,
+                                bootable=True,
+                                )
+    print("Created volume: ", json.dumps(volume))
+
+    # Create the server using the server_name parameter in the GET request
+    server_name = request.args.get('server_name')
+    print("Starting to create the server with name: ", server_name)
+    server = conn.create_server(name=server_name,
+                                flavor="m1.micro",
+                                terminate_volume=True,
+                                timeout=180,
+                                boot_volume=volume.id,
+                                key_name="mayank-public-key",
+                                )
+    print("Created server: ", json.dumps(server))
+
+    return "Server create request sent!"
 
 
 if __name__ == "__main__":
